e122fb58d9625155d9391a38ed4e07b2275c83c5
Name <email@exaple.ru>
2023-05-15 08:11:48 +0300

    Добавьте файлы проекта.

---
+++ BasicsC#/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicsC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName = "Dmitry";
+            Console.WriteLine(MyName);
+            Console.ReadKey();
+        }
+    }
+}
