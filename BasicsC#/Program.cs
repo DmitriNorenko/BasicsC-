e714b517c4955113065c8e74b7ae6cfe98634d59
Name <email@exaple.ru>
2023-05-15 08:33:57 +0300

    Добавление строк вывода.

--- BasicsC#/Program.cs
+++ BasicsC#/Program.cs
@@ -12,6 +12,9 @@ namespace BasicsC_
         {
             const string MyName = "Dmitry";
             Console.WriteLine(MyName);
+            Console.WriteLine("Привет,Мир!");
+            Console.WriteLine("Мне 21 год.");
+            Console.WriteLine($"Меня зовут {MyName}.");
             Console.ReadKey();
         }
     }
